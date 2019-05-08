049e44bb1ba0257ddaaa2245fd8619ffbd781b49
Leandro Cesquini Pereira <leandro@leandrocp.com.br>
2019-05-08 15:07:23 +0000

    add map to test

--- test/plug/builder_test.exs
+++ test/plug/builder_test.exs
@@ -17,7 +17,7 @@ defmodule Plug.BuilderTest do
 
     plug :fun, parent: builder_opts()
     plug Module, :opts
-    plug Module, %{opts: builder_opts()}
+    plug Module, %{opt: "test"}
 
     def fun(conn, opts) do
       stack = [{:fun, opts} | conn.assigns[:stack]]
@@ -92,11 +92,13 @@ defmodule Plug.BuilderTest do
     conn = assign(conn(:get, "/"), :stack, [])
 
     assert Sample.call(conn, []).assigns[:stack] == [
+             call: {:init, %{opt: "test"}},
              call: {:init, :opts},
              fun: [parent: []]
            ]
 
     assert Sample.call(conn, :parent).assigns[:stack] == [
+             call: {:init, %{opt: "test"}},
              call: {:init, :opts},
              fun: [parent: :parent]
            ]
